edb6da7b8c377c2a59eebce0b89a5b24ac71e6e4
Vincent Chen <chenvinc@oregonstate.edu>
2022-01-25 23:09:54 -0500

    created login skeleton UI

---
+++ frontend/src/Login.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import "./Login.css";
+import Button from "./components/Button";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <form className='form'>
+        <div className='user'>
+            <label>User: <input className='user-label' type='text'></input></label>
+        </div>
+        <div className='password'>
+            <label>Password: <input className='password-label' type='text'></input></label>
+        </div>
+        <div className='submit-btn-block'><input className="submit-btn" type="submit" value="Login" /></div>
+    </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Button from "./Button";
 import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <h1 style={{display:"block", paddingLeft: 30, paddingTop:10}}>Logo</h1>
+            <ul className='navitems'>
+                <li>Login</li>
+                <li>Signup</li>
+            </ul>
         </div>
     )
 }
