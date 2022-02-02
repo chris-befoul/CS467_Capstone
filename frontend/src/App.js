f641c60d537e01b2ab3289e9d638b2fd8dc9a5f1
Vincent Chen <chenvinc@oregonstate.edu>
2022-02-01 19:36:40 -0500

    moved routes and components to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Signup from './Signup';
+import UserSignup from './UserSignup';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/signup" element={<Signup />} />
+                <Route exact path="/usersignup" element={<UserSignup />} />
+                <Route exact path="/login" element={<Login />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,29 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Signup from './Signup';
-import UserSignup from './UserSignup';
-import Login from './Login';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/signup" element={<Signup />} />
-      <Route exact path="/usersignup" element={<UserSignup />} />
-      <Route exact path="/login" element={<Login />}/>
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
