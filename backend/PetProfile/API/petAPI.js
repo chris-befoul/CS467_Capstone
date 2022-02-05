6bbd540deef07f217a738ee9e69156af278f259b
liumiche <liumiche@oregonstate.edu>
2022-02-05 08:28:32 -0800

    midpoint deployment

--- backend/PetProfile/API/petAPI.js
+++ backend/PetProfile/API/petAPI.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-        callBack(null, 'uploads')
-    },
-    filename: (req, file, callBack) => {
-        callBack(null, `${file.originalname}`)
-    }
-})
-const upload = multer({ dest: 'uploads/' });
-const directory = 'uploads';
+const os = require('os');
+// const storage = multer.diskStorage({
+//     destination: (req, file, callBack) => {
+//         callBack(null, 'uploads')
+//     },
+//     filename: (req, file, callBack) => {
+//         callBack(null, `${file.originalname}`)
+//     }
+// })
+const upload = multer({ dest: os.tmpdir() });
+// const directory = 'uploads';
 const router = express.Router();
 router.use(bodyParser.json());
 
@@ -41,22 +42,22 @@ router.patch('/:petID', upload.array('file'), (req,res) => {
                     const fileName = key.id + '/' + (x + 1);
                     petPhotoFunction.uploadPhoto(req.files[x].path, fileName);
                 }
-                fs.readdir(directory, (err, files) => {
-                    if (err) throw err;
+                // fs.readdir(directory, (err, files) => {
+                //     if (err) throw err;
                 
-                    for (const file of files) {
-                    fs.unlink(path.join(directory, file), err => {
-                        if (err) throw err;
-                    });
-                    }
-                });   
+                //     for (const file of files) {
+                //     fs.unlink(path.join(directory, file), err => {
+                //         if (err) throw err;
+                //     });
+                //     }
+                // });   
             }
             res.status(201).send(key);
             return; });
         return;
 })
 
-router.post('/createPetProfile', upload.array('file'), (req, res) => {
+router.post('/createProfile', upload.array('file'), (req, res) => {
     const data = JSON.parse(req.body.data);
     if (!req.files) {
         const error = new Error('No File')
@@ -68,15 +69,14 @@ router.post('/createPetProfile', upload.array('file'), (req, res) => {
                     const fileName = key.id + '/' + (x + 1);
                     petPhotoFunction.uploadPhoto(req.files[x].path, fileName);
                 }
-                fs.readdir(directory, (err, files) => {
-                    if (err) throw err;
-                  
-                    for (const file of files) {
-                      fs.unlink(path.join(directory, file), err => {
-                        if (err) throw err;
-                      });
-                    }
-                  });
+                // fs.readdir(directory, (err, files) => {
+                //     if (err) throw err;
+                //     for (const file of files) {
+                //       fs.unlink(path.join(directory, file), err => {
+                //         if (err) throw err;
+                //       });
+                //     }
+                //   });
                 res.status(201).send(key);
                 return;
     })
