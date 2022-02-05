1468b4aed82d4649b675c04f95112d6e6d415525
liumiche <liumiche@oregonstate.edu>
2022-02-05 10:59:46 -0800

    Updated petProfile CSS to not affect other <p> tags

--- frontend/src/petProfile.js
+++ frontend/src/petProfile.js
@@ -8,7 +8,6 @@ import "./petProfile.css";
 const ViewPetProfile = () => {
     const params = useParams()
     const [petData, setData] = React.useState({});
-    const navigate = useNavigate();
     const fetchURL = 'http://localhost:8080';
     // const fetchURL = 'https://cs467-sandbox.ue.r.appspot.com';
     // const fetchURL = 'https://capstone-animal-adoption-app.wl.r.appspot.com';
@@ -34,16 +33,16 @@ const ViewPetProfile = () => {
 
     return (
         <div id='pet-profile'>
-            <p>Pet Name:            {petData.name}</p>
-            <p>Pet Type:            {petData.type}</p>
-            <p>Pet Breed:           {petData.breed}</p>
-            <p>Pet Sex:            {petData.sex}</p>
-            <p>Pet Age:             {petData.age}</p>
-            <p>Pet Weight:          {petData.weight}</p>
-            <p>Pet Availability:    {petData.availability}</p>
-            <p>Pet Disposition:     {petData.disposition}</p>
-            <p>Pet Description:     {petData.description}</p>
-            <p>Date Created:        {petData.date_created}</p>
+            <p className='pet-profile-field'>Pet Name:            {petData.name}</p>
+            <p className='pet-profile-field'>Pet Type:            {petData.type}</p>
+            <p className='pet-profile-field'>Pet Breed:           {petData.breed}</p>
+            <p className='pet-profile-field'>Pet Sex:            {petData.sex}</p>
+            <p className='pet-profile-field'>Pet Age:             {petData.age}</p>
+            <p className='pet-profile-field'>Pet Weight:          {petData.weight}</p>
+            <p className='pet-profile-field'>Pet Availability:    {petData.availability}</p>
+            <p className='pet-profile-field'>Pet Disposition:     {petData.disposition}</p>
+            <p className='pet-profile-field'>Pet Description:     {petData.description}</p>
+            <p className='pet-profile-field'>Date Created:        {petData.date_created}</p>
             <button id="edit-pet" onClick={editProfile}>Edit Pet Profile</button>
         </div>
     )
