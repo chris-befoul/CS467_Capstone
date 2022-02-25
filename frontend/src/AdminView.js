0eccc5c5d292326988f1dc67e5e80bdd036de703
Chris Hardison <chris.befoul@gmail.com>
2022-02-24 22:36:27 -0500

    Finished UI skeleton for Admin Page

--- frontend/src/AdminView.js
+++ frontend/src/AdminView.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { styled } from '@mui/material/styles';
-import { Grid, Paper, Typography, ButtonBase, Button } from "@mui/material";
+import { Grid, Typography, Button, Box } from "@mui/material";
 
 
 const AdminViewPage = () => {
     const [userList, setUsers] = React.useState([]);
+    const [filter, setFilter] = React.useState([]);
     const fetchURL = 'http://localhost:8080';
     // const fetchURL = 'https://cs467-sandbox.ue.r.appspot.com';
     // const fetchURL = 'https://capstone-animal-adoption-app.wl.r.appspot.com';
@@ -18,85 +18,95 @@ const AdminViewPage = () => {
 
     const BuildUsers = () => {
         if(userList.length > 0) {
-            return (<div>
+            return (
+            <div>
                 {userList.map((user) => {
-                if(user.type == "User") {
                     return <UserInfo user={user} key={user.id}/>
-                }
-                else if (user.type == "Shelter") {
-                    return <ShelterInto shelter={user} key={user.id} />
-                }
             })}
             </div>)
         }
         return <p>No Users Availabe</p>
     }
 
-    const ShelterInto = ({shelter}) => {
-        return <Paper>
-            <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-                        <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-                    {shelter.type}
-                </Typography>
-            <Typography variant="body1">
-                                Shelter Name: {shelter.shelter_name}
-                            </Typography>
-                            <Typography variant="body1">
-                                Location: {shelter.city}, {shelter.state} {shelter.zip_code}
-                            </Typography>
-                            <Typography variant="body1">
-                                Email: {shelter.email}
-                            </Typography>
-                            <Typography variant="body1">
-                                Phone: {shelter.phone}
-                            </Typography>
-                            </Grid>
-                            </Grid>
-                            <Grid item xs={3} container direction="column" justifyContent="center" alignItems="center">
-                        <Grid item p={2}>
-                            <Button variant="contained" color="error">Delete</Button>
-                        </Grid>
-                    </Grid>
-                            </Grid>
+    const ShelterName = ({shelter}) => {
+        return <Typography variant="body1">
+        Shelter Name: {shelter.shelter_name}
+    </Typography>
+    }
 
-        </Paper>
+    const UserName = ({user}) => {
+        return <Typography variant="body1">
+        Name:   {user.first_name} {user.last_name}
+    </Typography>
     }
 
     const UserInfo = ({user}) => {
-        return <Paper>
-            <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-                        <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-                    {user.type}
-                </Typography>
-            <Typography variant="body1">
-                                Name:   {user.first_name} {user.last_name}
-                            </Typography>
-                            <Typography variant="body1">
-                                Location:   {user.city}, {user.state} {user.zip_code}
-                            </Typography>
-                            <Typography variant="body1">
-                                Email:  {user.email}
-                            </Typography>
-                            <Typography variant="body1">
-                                Phone:  {user.phone}
-                            </Typography>
+        var userName = true;
+        if(user.type == "Shelter") {
+            userName = false
+        } 
+        return (
+                    <Grid container>
+                        {/* <Grid xs={11} > */}
+                            <Grid container xs={8} item >
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {user.type}
+                                </Typography>
+                                <Grid container >
+                                    <Grid item >
+                                        {!userName ? <ShelterName shelter={user} key={user.id} /> : null}
+                                        {userName ? <UserName user={user} key={user.id} /> : null}
+                                    </ Grid>
+                                    <Grid item >
+                                        <Typography variant="body1">
+                                            Location:   {user.city}, {user.state} {user.zip_code}
+                                        </Typography>
+                                    </ Grid>
+                                    <Grid item >
+                                        <Typography variant="body1">
+                                            Email:  {user.email}
+                                        </Typography>
+                                    </ Grid>
+                                    <Grid item >
+                                        <Typography variant="body1">
+                                            Phone:  {user.phone}
+                                        </Typography>
+                                    </ Grid>
+                                </Grid>
                             </Grid>
+                        {/* </Grid> */}
+                        <Grid container item xs={2} justifyContent="center" alignItems="center">
+                            <Grid p={2} >
+                                <Button variant="contained" color="error">Delete</Button>
                             </Grid>
-                            <Grid item xs={3} container direction="column" justifyContent="center" alignItems="center">
-                        <Grid item p={2}>
-                            <Button variant="contained" color="error">Delete</Button>
                         </Grid>
                     </Grid>
-                    </Grid>
-        </Paper>
+                )
+    }
+
+    const FilterBox = () => {
+        return (
+                <Grid item xs={2}>
+                    <div >
+                        <h3>Filter By Type</h3>
+                        <div id='filter-box'>
+                            <input type="radio" id="user" name="Type" value="User" onChange={e => setFilter(e.target.value)}/>
+                            <label for="user">User</label><br />
+                            <input type="radio" id="shelter" name="Type" value="Shelter" onChange={e => setFilter(e.target.value)}/>
+                            <label for="shelter">Shelter</label><br />
+                        </div>
+                    </div>
+                </Grid>
+        )
     }
 
     return (
-        <BuildUsers />
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container>
+                <FilterBox />
+                <BuildUsers />
+            </Grid>
+        </Box>
     )
 }
 
