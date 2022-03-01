49e759b97ace3ffaabc5ab3fea4649a2a71ba72e
liumiche <liumiche@oregonstate.edu>
2022-03-01 12:50:50 -0800

    Redirect featured pet to pet profile on click

--- frontend/src/components/FeaturedPet.js
+++ frontend/src/components/FeaturedPet.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Grid, Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const FeaturedPet = ({pet}) => {
     const photoURL = 'https://storage.googleapis.com/pet_profile_photos/';
+    const nagivate = useNavigate();
 
     return (
         <Grid item md={3}>
             <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
+                <CardActionArea onClick={() => nagivate('/pets/viewProfile/' + pet.id)}>
                     <CardMedia
                         component="img"
                         height='300'
