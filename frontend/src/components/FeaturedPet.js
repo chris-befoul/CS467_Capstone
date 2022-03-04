535e403302633fcd944e8ff08d6d7b21693f2e0b
liumiche <liumiche@oregonstate.edu>
2022-03-03 20:08:26 -0800

    minor styling changes

--- frontend/src/components/FeaturedPet.js
+++ frontend/src/components/FeaturedPet.js
@@ -7,7 +7,7 @@ const FeaturedPet = ({pet}) => {
     const nagivate = useNavigate();
 
     return (
-        <Grid item m={2}>
+        <Grid item md={3} marginBottom={10}>
             <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea onClick={() => nagivate('/pets/viewProfile/' + pet.id)}>
                     <CardMedia
